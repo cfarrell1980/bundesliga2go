5e90f3a29a5cf9a84b3d0809eef38420ad143e43
Ciaran Farrell <cfarrell1980@gmail.com>
2011-01-30 04:29:34 +0100

    move the coding declaration back up to the first line, where it belongs

--- OpenLigaDB.py
+++ OpenLigaDB.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 '''
 Copyright (c) 2011, Ciaran Farrell, Vladislav Gorobets
 All rights reserved.
@@ -28,7 +30,6 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 '''
 
-# -*- coding: utf-8 -*-
 from suds.client import Client
 from suds import sudsobject
 
