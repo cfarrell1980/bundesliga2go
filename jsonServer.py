4aa868a90832301b76ecd472199e0e60f9665be3
Ciaran Farrell <cfarrell1980@gmail.com>
2011-02-01 17:08:00 +0100

    no-cache on getData

--- jsonServer.py
+++ jsonServer.py
@@ -196,8 +196,8 @@ class getData:
     season = season.season
     if not season: season = current_bundesliga_season()
     tstamp = web.input(tstamp=None)
-    web.header('Cache-Control','no-cache')
-    web.header('Pragma','no-cache')
+    #web.header('Cache-Control','no-cache')
+    #web.header('Pragma','no-cache')
     web.header('Content-Type','application/json')
     if tstamp.tstamp:
       try:
