6e3ae5db91a26cf852b0778138d6e9564c0db3d8
Ciaran Farrell <cfarrell1980@gmail.com>
2011-02-04 09:32:52 +0100

    shortcuts added where they were missing

--- bundesligaHelpers.py
+++ bundesligaHelpers.py
@@ -44,7 +44,8 @@ shortcuts = {40: 'FCB',9:'S04',134:'BRE',6:'B04',16:'FVB',7:'BVB',
              65:'1FCK',98:'FCP',123:'HOF',79:'1FCN',55:'H96',100:'HSV',
              54:'BSC',129:'BOC',105:'KSC',83:'DSC',93:'FCE',125:'M60',
              185:'F95',23:'AAC',107:'MSV',95:'FCA',173:'RWO',80:'FCU',
-             66:'AUE',36:'OSN',114:'SGF',31:'SCP',171:'FCI',172:'FSV'
+             66:'AUE',36:'OSN',115:'SGF',31:'SCP',171:'FCI',172:'FSV',
+             29:'OFF',69:'CZJ',127:'TUK',174:'SWW',102:'ROS'
             }
 
 def json_serialize(obj):
