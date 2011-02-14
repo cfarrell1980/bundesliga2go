2bbbd0dc061921c17613eb6b3755876b607f3139
Ciaran Farrell <cfarrell1980@gmail.com>
2011-02-14 23:35:39 +0100

    If the hardcoded directory doesn't exist, use cwd

--- bundesligaLogger.py
+++ bundesligaLogger.py
@@ -1,5 +1,7 @@
 import logging,os
 here = "/space/repos/bundesliga2go/"
+if not os.path.isdir(here):
+  here = os.getcwd()
 logfile = os.path.abspath(os.path.join(here,"bundesliga2go.log"))
 logger = logging.getLogger('bundesliga2go')
 hdlr = logging.FileHandler(logfile)
