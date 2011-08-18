2d7d9df41703c565057950dad1ebd9a9692b067a
Ciaran Farrell <cfarrell1980@gmail.com>
2011-08-18 23:10:03 +0200

    New Schema

--- bundesligaORM.py
+++ bundesligaORM.py
@@ -13,7 +13,7 @@ except ImportError:
     raise ImportError, "Tried using sqlalchemy.orm.relationship and sqlalchemy.orm.relation"
 import datetime,os
 
-dbfile = os.path.join(os.getcwd(),"bundesliga.sqlite")
+dbfile = os.path.join(os.getcwd(),"bundesliga2.sqlite")
 engine = create_engine("sqlite:///%s"%dbfile,echo=False)
 metadata = MetaData()
 Session = sessionmaker(bind=engine)
@@ -21,116 +21,88 @@ Session = sessionmaker(bind=engine)
 def now():
   return datetime.datetime.now()
 
-league_table = Table('league', metadata,
-  Column('id', Integer, primary_key=True),
-  Column('name', String),
-  Column('season', Integer),
-  Column('mtime',DateTime,default=now(), onupdate=now())
-)
-
-class League(object):
-  def __init__(self,name,season):
-    self.name = name
-    self.season = season
-
-  def __repr__(self):
-    return "<League('%s','%d')>"%(self.name,self.season)
-
 match_table = Table('match',metadata,
   Column('id', Integer, primary_key=True),
-  Column('startTime', DateTime),
-  Column('endTime', DateTime),
-  Column('isFinished',Boolean),
-  Column('matchday', Integer),
-  Column('viewers',Integer,default=0),
-  Column('pt1',Integer,default=0),
-  Column('pt2',Integer,default=0),
-  Column('league_id',Integer,ForeignKey('league.id')),
-  Column('mtime', DateTime,default=now(),onupdate=now())
+  Column('matchTeam1',Integer,ForeignKey('team.id')),
+  Column('matchTeam2',Integer,ForeignKey('team.id')),
+  Column('matchStartTime', DateTime),
+  Column('matchIsFinished',Boolean),
+  Column('matchMatchday', Integer),
+  Column('matchViewers',Integer,default=0),
+  Column('matchPointsTeam1',Integer,default=0),
+  Column('matchPointsTeam2',Integer,default=0),
+  Column('matchLocation',String)
 )
 
 class Match(object):
-  def __init__(self,id,matchday,startTime,endTime,isFinished,pt1,pt2,viewers=0):
-    self.id = id
-    self.startTime = startTime
-    self.endTime = endTime
-    self.isFinished = isFinished
-    self.viewers = viewers
-    self.matchday = matchday
-    self.pt1 = pt1
-    self.pt2 = pt2
-
+  def __init__(self,matchID,matchStartTime,
+               matchIsFinished,matchMatchday,matchViewers=0,matchPointsTeam1=0,
+               matchPointsTeam2=0,matchLocation=None):
+    self.id = matchID
+    self.matchStartTime = matchStartTime
+    self.matchIsFinished = matchIsFinished
+    self.matchMatchday = matchMatchday
+    self.matchViewers = matchViewers
+    self.matchPointsTeam1 = matchPointsTeam1
+    self.matchPointsTeam2 = matchPointsTeam2
+    self.matchLocation = matchLocation
   def __repr__(self):
-    return "<Match('%d','%s','%s','%s','%d','%d','%d','%d')>"%(self.id,self.startTime,
-      self.endTime,self.isFinished,self.viewers,self.matchday,self.pt1,self.pt2)
+    return "<Match('%d','%s')>"%(self.id,self.matchStartTime)
 
 goal_table = Table('goal',metadata,
   Column('id', Integer, primary_key=True),
-  Column('scorer', String),
-  Column('minute', Integer),
-  Column('penalty', Boolean),
-  Column('t1score', Integer),
-  Column('t2score', Integer),
-  Column('for_team_id', Integer, ForeignKey('team.id')),
-  Column('match_id', Integer, ForeignKey('match.id')),
-  Column('ownGoal', Boolean,default=False),
-  Column('mtime',DateTime,default=now(),onupdate=now())
+  Column('goalScorer', String),
+  Column('goalMinute', Integer),
+  Column('goalIsPenalty', Boolean,default=False),
+  Column('goalIsOwnGoal', Boolean,default=False),
+  Column('match_id',Integer,ForeignKey('match.id')),
+  Column('team_id',Integer,ForeignKey('team.id')),
+  Column('goalMatchModified',DateTime,default=now(),onupdate=now())
   )
 
 class Goal(object):
-  def __init__(self,id,scorer,minute,t1,t2,og,penalty=False):
-    self.id = id
-    self.scorer = u"%s"%scorer.decode('utf-8')
-    self.minute = minute
-    self.penalty = penalty
-    self.t1score = t1
-    self.t2score = t2
-    self.ownGoal = og
+  def __init__(self,goalID,goalScorer,goalMinute,goalIsPenalty,goalIsOwnGoal):
+    self.id = goalID
+    self.goalScorer = u"%s"%goalScorer.decode('utf-8')
+    self.goalMinute = goalMinute
+    self.goalIsPenalty = goalIsPenalty
+    self.goalIsOwnGoal = goalIsOwnGoal
+    self.goalMatchModified = now()
 
   def __repr__(self):
-    return "<Goal('%d','%s','%s','%s','%s')>"%(self.id,self.scorer,self.minute,self.penalty,self.ownGoal)
+    return "<Goal('%d','%s','%d','Penalty?:%s','OwnGoal?:%s')>"%(self.id,self.goalScorer,self.goalMinute,
+             self.goalIsPenalty,self.goalIsOwnGoal)
 
 team_table = Table('team',metadata,
   Column('id',Integer,primary_key=True),
-  Column('name',String),
-  Column('iconURL',String),
-  Column('mtime',DateTime,default=now(),onupdate=now())
+  Column('teamShortName',String),
+  Column('teamFullName',String),
+  Column('teamIconUrl',String),
   )
 
 class Team(object):
-  def __init__(self,id,name,iconURL):
-    self.id = id
-    self.name = u"%s"%name.decode('utf-8')
-    self.iconURL = iconURL
+  def __init__(self,teamID,teamFullName,teamIconUrl,teamShortName=None):
+    self.id = teamID
+    self.teamShortName = teamShortName
+    self.teamFullName = u"%s"%teamFullName.decode('utf-8')
+    self.teamIconUrl = teamIconUrl
 
   def __repr__(self):
-    return "<Team('%d','%s','%s')>"%(self.id,
-                  self.name.encode('utf-8'),self.iconURL)
+    return "<Team('%d','%s','%s','%s')>"%(self.id,self.teamShortName,
+                  self.teamFullName.encode('utf-8'),self.teamIconUrl)
 
-teams_leagues = Table('teams_leagues', metadata,
-  Column('team_id', Integer, ForeignKey('team.id')),
-  Column('league_id', Integer, ForeignKey('league.id')),
-  Column('mtime',DateTime,default=now(),onupdate=now())
-)
 
-teams_matches = Table('teams_matches', metadata,
-  Column('team_id', Integer, ForeignKey('team.id')),
-  Column('match_id', Integer, ForeignKey('match.id')),
-  Column('mtime',DateTime,default=now(),onupdate=now())
-)
-
-mapper(League,league_table,properties={
-  'teams': relationship(Team,secondary=teams_leagues,backref='leagues'),
-  'matches':relationship(Match,backref='league')
+mapper(Team, team_table,properties={
+  'goals':relationship(Goal,backref='team'),
 })
 
 mapper(Match,match_table,properties={
-  'teams': relationship(Team,secondary=teams_matches,backref='matches'),
+  'team1':relationship(Team,foreign_keys=[match_table.c.matchTeam1],primaryjoin=match_table.c.matchTeam1==team_table.c.id),
+  'team2':relationship(Team,foreign_keys=[match_table.c.matchTeam2],primaryjoin=match_table.c.matchTeam2==team_table.c.id),
   'goals':relationship(Goal,backref='match'),
 })
 
 mapper(Goal,goal_table)
 
-mapper(Team, team_table,properties={'goals':relationship(Goal,backref='team')})
 
 metadata.create_all(engine)
