b9f451e6bc8f9fb0fa6a2a71eaf67c0405c10d2f
Ciaran Farrell <cfarrell1980@gmail.com>
2011-08-24 22:18:03 +0200

    Added shortnames for Dresden and Braunschweig

--- PermaData.py
+++ PermaData.py
@@ -66,6 +66,9 @@ def getDefaultSeasons():
   else:
     seasons = [getCurrentSeason(),getCurrentSeason()-1]
   return seasons
+  
+def getDefaultLeague():
+  return DEFAULT_LEAGUE
 
 DEFAULT_SEASONS = getDefaultSeasons()  
 DEFAULT_SEASON = DEFAULT_SEASONS[0]
@@ -76,7 +79,8 @@ teamShortcuts = {40: 'FCB',9:'S04',134:'BRE',6:'B04',16:'FVB',7:'BVB',
              54:'BSC',129:'BOC',105:'KSC',83:'DSC',93:'FCE',125:'M60',
              185:'F95',23:'AAC',107:'MSV',95:'FCA',173:'RWO',80:'FCU',
              66:'AUE',36:'OSN',115:'SGF',31:'SCP',171:'FCI',172:'FSV',
-             29:'OFF',69:'CZJ',127:'TUK',174:'SWW',102:'ROS'
+             29:'OFF',69:'CZJ',127:'TUK',174:'SWW',102:'ROS',74:'EBS',
+             177:'SGD',
             }
 
 current_season = getCurrentSeason()
