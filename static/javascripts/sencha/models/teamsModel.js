87365fefae2a5b844da5d39921b66eb928c08f54
Ciaran Farrell <cfarrell1980@gmail.com>
2011-09-14 13:09:58 +0200

    restarting with mongo

--- static/javascripts/sencha/models/teamsModel.js
+++
@@ -1,8 +0,0 @@
-TeamsApp.models.Team = Ext.regModel('TeamModel', {
-    fields: [
-        {name: 'teamID',       type: 'string'},
-        {name: 'teamName',     type: 'string'},
-        {name: 'teamIconUrl',  type: 'string'}
-    ]
-});
-
